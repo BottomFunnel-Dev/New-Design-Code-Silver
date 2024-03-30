001936f12fe06801fcafa17eb3e6b26458f0fd06
Aaradhana29 <kumariaaradhana99@gmail.com>
2024-03-30 12:35:39 +0530

    changes in banner component

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,9 +1,9 @@
-
+import { HomeBanner } from "../../components/Homepage/homeBanner/homeBanner";
 export default function Homepage() {
  
   return (
     <div>
-     
+       <HomeBanner/>
 
     </div>
   );
